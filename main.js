110a249ba7d09a8d3791fbfad5dd00d158d3a646
Al <alsami1209@gmail.com>
2024-03-31 16:57:03 +0600

    not done yet done

---
+++ main.js
@@ -0,0 +1,9 @@
+let btn1 = document.getElementById("myBtn");
+let btn2 = document.getElementById("myBtn2");
+
+const handleButton1 = () => {
+    
+};
+
+btn1.addEventListener("click", handleButton1)
+btn2.addEventListener("click", handleButton2)
